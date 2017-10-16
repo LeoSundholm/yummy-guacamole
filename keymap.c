6cf012128f793855137a4cb89040095e357b43b9
Leo Sundholm <leo@tocaboca.com>
2017-10-16 14:32:55 +0200

    removed outdated svorak config, added current one

--- keymap.c
+++ keymap.c
@@ -1,12 +1,12 @@
-#include "extended_keymap_common.h"
+#include "keymap.h"
 #include "action_layer.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = { /* Base */
-  {KC_ESC,	 KC_LBRACKET,	KC_QUOTE,	  KC_SCOLON,	KC_P,	   KC_Y,	 KC_F,  	KC_G,	    KC_C,	    KC_R,	    KC_L,	    KC_BSPC},	
-  {FUNC(18), KC_A,	      KC_O,	      KC_E,	      KC_U,  	 KC_I,	 KC_D,	  KC_H,	    KC_T,	    KC_N,	    KC_S,	    KC_ENT},	
-  {KC_LSFT,	 KC_DOT,	    KC_Q,	      KC_J,	      KC_K,	   KC_X,	 KC_B,	  KC_M,	    KC_W,	    KC_V,	    KC_Z,	    KC_COMMA},	
-  {KC_LCTL,	 KC_LALT,	    KC_LGUI,	  FUNC(3),	  FUNC(2), KC_SPC, KC_SPC,	FUNC(1),	FUNC(3),	KC_LGUI,	KC_LALT,	KC_LCTL}
+  {KC_ESC,   KC_LBRACKET, KC_QUOTE,   KC_SCOLON,  KC_P,    KC_Y,   KC_F,    KC_G,     KC_C,     KC_R,     KC_L,     KC_BSPC}, 
+  {FUNC(18), KC_A,        KC_O,       KC_E,       KC_U,    KC_I,   KC_D,    KC_H,     KC_T,     KC_N,     KC_S,     KC_ENT},  
+  {KC_LSFT,  KC_DOT,      KC_Q,       KC_J,       KC_K,    KC_X,   KC_B,    KC_M,     KC_W,     KC_V,     KC_Z,     KC_COMMA},  
+  {KC_LCTL,  KC_LALT,     KC_LGUI,    FUNC(3),    FUNC(2), KC_SPC, KC_SPC,  FUNC(1),  FUNC(3),  KC_LGUI,  KC_LALT,  KC_LCTL}
   },
 [1] = { /* QWERTY Base (lol åäö är typ borta orka vara seriös)*/
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
@@ -21,21 +21,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_LCTL, KC_LALT, KC_LGUI, FUNC(3), FUNC(2), KC_SPC,  KC_SPC,  FUNC(1), FUNC(3), KC_LGUI, KC_LALT, KC_LCTL}
 },
 [3] = { /* Raise */
-  {LALT(KC_RBRACKET),	KC_1,	    KC_2,	    KC_3,	    KC_4,	    KC_5,	    KC_6,	    KC_7,	    KC_8,	    KC_9,	   KC_0,	            LSFT(KC_RBRACKET)},
-  {KC_TRNS,           KC_F1,	  KC_F2,	  KC_F3,	  KC_F4,	  KC_F5,	  KC_F6,    KC_4, 	  KC_5,	    KC_6,    LSFT(KC_RBRACKET), KC_TRNS},	
-  {KC_TRNS, 	        KC_F7,	  KC_F8,	  KC_F9,	  KC_F10,	  KC_F11,	  KC_0,	    KC_1,   	KC_2,   	KC_3,	   KC_F12,            KC_TRNS},	
-  {KC_TRNS,         	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS, KC_TRNS,           KC_TRNS}
+  {LALT(KC_RBRACKET), KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,    KC_0,              LSFT(KC_RBRACKET)},
+  {KC_TRNS,           KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_4,     KC_5,     KC_6,    LSFT(KC_RBRACKET), KC_TRNS}, 
+  {KC_TRNS,           KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_0,     KC_1,     KC_2,     KC_3,    KC_F12,            KC_TRNS}, 
+  {KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,           KC_TRNS}
 },
 [4] = { /* Lower */
-  {LSFT(KC_EQUAL), LSFT(KC_1),	 LSFT(KC_2),	    LSFT(KC_3),	LALT(KC_4),	    LSFT(KC_5),	      LSFT(KC_6),	LSFT(KC_7),	LSFT(KC_8),	      LSFT(KC_9),	      LSFT(KC_0),   	KC_MINUS},	
-  {KC_EQUAL,       LALT(KC_2),	 LSFT(KC_SLASH),	KC_GRAVE,	  LSFT(KC_GRAVE),	LSFT(LALT(KC_7)),	LALT(KC_7),	KC_BSLASH,	LSFT(LALT(KC_8)),	LSFT(LALT(KC_9)),	LSFT(KC_MINUS),	KC_SLASH},
-  {KC_TRNS,	       LSFT(KC_DOT), FUNC(10),	      FUNC(8),	  FUNC(9),	      KC_TRNS,	        KC_TRNS,	  KC_COMMA,	  LALT(KC_8),	      LALT(KC_9),	      KC_TRNS,	      LSFT(KC_BSLASH)},	
-  {KC_TRNS,	       KC_TRNS,	     KC_TRNS,	        KC_TRNS,	  KC_TRNS,	      KC_TRNS,	        KC_TRNS,	  KC_TRNS,	  KC_TRNS,	        KC_TRNS,        	KC_TRNS,        KC_TRNS}
+  {LSFT(KC_EQUAL), LSFT(KC_1),   LSFT(KC_2),      LSFT(KC_3), LALT(KC_4),     LSFT(KC_5),       LSFT(KC_6), LSFT(KC_7), LSFT(KC_8),       LSFT(KC_9),       LSFT(KC_0),     KC_MINUS},  
+  {KC_EQUAL,       LALT(KC_2),   LSFT(KC_SLASH),  KC_GRAVE,   LSFT(KC_GRAVE), LSFT(LALT(KC_7)), LALT(KC_7), KC_BSLASH,  LSFT(LALT(KC_8)), LSFT(LALT(KC_9)), LSFT(KC_MINUS), KC_SLASH},
+  {KC_TRNS,        LSFT(KC_DOT), FUNC(10),        FUNC(8),    FUNC(9),        KC_TRNS,          KC_TRNS,    KC_COMMA,   LALT(KC_8),       LALT(KC_9),       KC_TRNS,        LSFT(KC_BSLASH)}, 
+  {KC_TRNS,        KC_TRNS,      KC_TRNS,         KC_TRNS,    KC_TRNS,        KC_TRNS,          KC_TRNS,    KC_TRNS,    KC_TRNS,          KC_TRNS,          KC_TRNS,        KC_TRNS}
 },
 [5] = { /* META */
   {KC_MPLY,  KC_HOME,  KC_UP,    KC_END,   KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_HOME,  KC_UP,    KC_END,   KC_TRNS, KC_DELETE}, 
-  {KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGUP,    KC_TRNS,  KC_PGUP,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS, KC_TRNS},
-  {KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDOWN,  KC_TRNS, KC_PGDOWN,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MPRV, KC_MNXT},  
+  {KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGUP,    KC_TRNS,  KC_PGUP,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS, FUNC(28)},
+  {KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGDOWN,  KC_TRNS,  KC_PGDOWN,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MPRV, KC_MNXT},  
   {KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  FUNC(6),  FUNC(4), FUNC(5)}
 }
 };
@@ -46,6 +46,7 @@ enum macro_id {
     PASTE,
     CUT,
     UNDO,
+    TOCABIKE,
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -80,6 +81,10 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
             return (event.pressed ?
                     MACRO( D(LGUI), T(Z), U(LGUI), END ) :
                     MACRO( END ) );
+        case TOCABIKE:
+           return (event.pressed ?
+                    MACRO( D(LSFT), T(DOT), U(LSFT), T(T), T(O), T(C), T(A), T(B), T(I), T(K), T(E), D(LSFT), T(DOT), U(LSFT), T(SPC), END ) : 
+                    MACRO( END ) );
     }
     return MACRO_NONE;
 }
@@ -99,6 +104,7 @@ const uint16_t PROGMEM fn_actions[] = {
     [9] = ACTION_MACRO(PASTE), 
     [10] = ACTION_MACRO(CUT), 
     [27] = ACTION_MACRO(UNDO), 
+    [28] = ACTION_MACRO(TOCABIKE), 
 
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
     [12] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
